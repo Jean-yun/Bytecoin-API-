60bf7274244d284a8b3cafe7e028d60825d2b8c0
Jean-yun <yesirchloe@gmail.com>
2024-07-16 12:13:38 +0900

    UI Change

--- Bytecoin(API)/Controller/ViewController.swift
+++ Bytecoin(API)/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -22,23 +22,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
         coinManager.delegate = self
     }
+}
 
-    // UI 업데이트는 MAIN에서 해야된다 !
-    func didUpdatePrice(price: String, currency: String) {
-        DispatchQueue.main.async {
-            self.bitcoinLabel.text = price
-            self.currencyLabel.text = currency
-        }
-    }
+//MARK: - UIPickerViewDataSource & Delegate
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
-    func didFailWithError(error: Error) {
-        print(error)
-    }
-
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
@@ -53,7 +44,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         print(coinManager.getCoinPrice(for: selectedCurrency))
     }
     
-    
-
 }
 
+//MARK: - CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
+    // UI 업데이트는 MAIN에서 해야된다 !
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
